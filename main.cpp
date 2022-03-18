117160fa9214fd8f7ec9494fb67f56e8eda283d9
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-18 13:53:04 -0600

    Final commit

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 int main() {
     int numero;
-    int numerosPrimos;
+    int contador;
+    int numerosPrimos = 1;
     cout << "Cuantos numeros desea calcular?" << endl;
     cin >> numero;
-    cout << numero << endl;
-    
+    cout << "Calculando " << numero << " numeros primos" << endl;
+    for(int i = 2; numerosPrimos <= numero; i++) {
+        contador = 0;
+        for(int j = 1; j <= i; j++) {
+            if(i % j == 0) {
+                contador++;
+            }
+        }
+        if(contador == 2) {
+            cout << numerosPrimos << ".- " << i << endl;
+            numerosPrimos++;
+        }
+    }
     return 0;
 }
