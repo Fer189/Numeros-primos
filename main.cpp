3d6b38f06bbb1232a4d012f9522214a5cb1a1021
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-18 13:22:56 -0600

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    int numerosPrimos;
+    cout << "Cuantos numeros desea calcular?" << endl;
+    cin >> numero;
+    cout << numero << endl;
+    
+    return 0;
+}
